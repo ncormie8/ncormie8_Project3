a2aac01bf93dac1dafea74795c8c46d89303da0c
ncormie8 <ncormie8@uwo.ca>
2024-11-21 11:19:25 -0500

    Trying to figure out how to setup eqn
    Struggling but slowly iterating what I believe to be better versions of the diffEQNs() function.

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -14,17 +14,22 @@ R0 = (7.72*(10**8))/(u_e)      # [cm]
 M0 = (5.67*(10**33))/(u_e**2)  # [g]
 
 # 10 inital values of rho to calculate solutions for (given)
-rho_c = np.linspace(1/10.,2.5*(10**6),10)
-
-# gamma
-# x = (rho/p0)**2
-# gam = 
+rho_c = np.linspace(1/10.,2.5*(10**6),10)  # already made unitless (p/p0)
 
 # function which returns equations 8 and 9 to be solved with solve_ivp
 def diffEQNs(r, f, gamma, ue):
-    ''''''
-    m, rho = f
-    gamma = ((rho/p0)**(2/3.))/((3.*(1+(rho/p0)**(2/3.)))**(1/2.))
+    '''r is independent variable
+       rho and m are dependent variables
+       '''
+    # constants
+    u_e = 2.
+    R0 = (7.72e8)/(u_e)      # [cm]
+    M0 = (5.67e33)/(u_e**2)  # [g]
+    
+    rho, m = f[0], f[1]      # setting rho and m equal to input indcies of f 
+    m = mi/M0
+    r = r/R0
+    gamma = ((rho)**(2/3.))/((3.*(1+(rho)**(2/3.)))**(1/2.))
     drho_dr = (-m*rho)/(gamma*(r**2))
     dm_dr = (r**2)*rho 
     return drho_dr , dm_dr
