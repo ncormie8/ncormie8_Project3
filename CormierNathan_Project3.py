7e31ad19fd1dd2915115e5cf7db7aefae165950b
ncormie8 <ncormie8@uwo.ca>
2024-11-20 21:26:32 -0500

    Added gamma(x) to function + constants
    Constructed gamma inside diffEQNs() to be calculated for each value of rho. Added rho0, R0, and M0 so that I can solve the equations with the correct values

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -9,14 +9,23 @@ import matplotlib.pyplot as plt
 
 # given values
 u_e = 2.
+p0 = (9.47*(10**5))*(u_e)      # [cm^-3]
+R0 = (7.72*(10**8))/(u_e)      # [cm]
+M0 = (5.67*(10**33))/(u_e**2)  # [g]
 
 # 10 inital values of rho to calculate solutions for (given)
 rho_c = np.linspace(1/10.,2.5*(10**6),10)
+
+# gamma
+# x = (rho/p0)**2
+# gam = 
 
 # function which returns equations 8 and 9 to be solved with solve_ivp
 def diffEQNs(r, f, gamma, ue):
     ''''''
     m, rho = f
+    gamma = ((rho/p0)**(2/3.))/((3.*(1+(rho/p0)**(2/3.)))**(1/2.))
     drho_dr = (-m*rho)/(gamma*(r**2))
     dm_dr = (r**2)*rho 
     return drho_dr , dm_dr
+
