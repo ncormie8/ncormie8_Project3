fc01af7cb47789f7728ba803db3a0cd50e61ccb1
ncormie8 <ncormie8@uwo.ca>
2024-11-21 12:57:16 -0500

    Tested and verified gammaX
    Created gammaX to calculate the value of gamma for each iteration of rho, and tested that the values were being computed properly between functions.

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 # given values
 
-# p0 = (9.47*(10**5))*(u_e)      # [cm^-3]
+# p0 = (9.47*(10**5))*(u_e)      # [g cm^-3]
 # R0 = (7.72*(10**8))/(u_e)      # [cm]
 # M0 = (5.67*(10**33))/(u_e**2)  # [g]
 
@@ -17,6 +17,17 @@ import matplotlib.pyplot as plt
 rho_c = np.linspace(1/10.,2.5*(10**6),10)  # already made unitless (p/p0)
 initial_m = 0
 initial_r = 1e-36
+
+def gammaX(rho):
+   u_e = 2.
+   p0 = (9.47e5)*(u_e)
+   print(rho,'gammaX rho')
+   print(p0)
+   rho_unitless = (rho/p0)**(1.0/3.0)
+
+   gamma = (rho_unitless**2)/(3*(1+rho_unitless**2)**(0.5))
+   print(gamma)
+   return gamma
 
 # function which returns equations 8 and 9 to be solved with solve_ivp
 def diffEQNs(r, f):
@@ -30,22 +41,22 @@ def diffEQNs(r, f):
    # setting each ode to an element of f
    m = f[0]
    rho = f[1]
-
+   print(rho,'diffeqns rho')
    # defining gamma
-   gamma = ((rho)**(2/3.))/((3.*(1+(rho)**(2/3.)))**(1/2.))
+   gam = gammaX(rho)
 
    # defining each ode
-   drho_dr = (-m*rho)/(gamma*(r**2))
+   drho_dr = (-m*rho)/(gam*(r**2))
    dm_dr = (r**2)*rho
 
    # returning eqns as appended array for easier operation
    out = np.append(dm_dr,drho_dr)
    return out
 
-f0 = [initial_m,rho_c[5]]
+f0 = [initial_m,rho_c[5]]            # m(r=0) = 0 | rho(r=0) = rho_c 
 print(diffEQNs(r=initial_r,f=f0))
 
-soln = integrate.solve_ivp(diffEQNs,(initial_r,100),f0)
+# soln = integrate.solve_ivp(diffEQNs,(initial_r,100),f0, )
 # sol = integrate.solve_ivp(diffEQNs,(0.0001,10),(rho_c[0],initial_m))
 
 # # constant
