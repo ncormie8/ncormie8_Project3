84f004c45e662b53cc4ddba49e66ebef5af3db52
ncormie8 <ncormie8@uwo.ca>
2024-11-21 17:29:37 -0500

    Added dimensionless function, modified gamma function
    Added function dimensionless() which intakes m, r, and rho and returns dimensionless verisions of the input quantites.
    Modified gammaX to use dimensionless function.

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -18,8 +18,22 @@ rho_c = np.linspace(1/10.,2.5*(10**6),10)  # already made unitless (p/p0)
 initial_m = 0.
 initial_r = 1e-10
 
+# function that returns dimensionless quantitues for r, m, and rho
+def dimensionless(r,m,rho):
+   u_e = 2.
+   p0 = (9.47*(10**5))*(u_e)      # [g cm^-3]
+   R0 = (7.72*(10**8))/(u_e)      # [cm]
+   M0 = (5.67*(10**33))/(u_e**2)  # [g]
+   
+   R = np.abs(r/R0)
+   M = np.abs(m/M0)
+   Rho = np.abs(rho/p0)
+   return [R,M,Rho]
+
+# function that calculates gamma for an input value of rho
 def gammaX(rho):
-   gamma = (rho**2)/(3*(1+rho**2)**(0.5))
+   x = dimensionless(0,0,rho)[2]**(1/3)
+   gamma = (x**2)/(3*(1+x**2)**(0.5))
    return gamma
 
 # function which returns equations 8 and 9 to be solved with solve_ivp
@@ -28,17 +42,17 @@ def diffEQNs(r, f):
    rho and m are dependent variables coorespoinding to indices [0] and [1] of f
    and must be unitless inputs (rho = rho/p0 , m = m/m0),
    '''
-
+   
+   
    # setting each ode to an element of f
-   m = f[0]
-   rho = f[1]
+   m , rho = f
    
    # defining gamma
    gam = gammaX(rho)
 
    # defining each ode
-   dm_dr = (r**2)*rho
    drho_dr = (-m*rho)/(gam*(r**2))
+   dm_dr = (r**2)*rho
 
    # returning eqns as appended array for easier operation
    out = np.append(dm_dr,drho_dr)
