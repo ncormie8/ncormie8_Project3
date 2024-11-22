d59903a459f75c140a6c4f723bb3e03a16c0d13c
ncormie8 <ncormie8@uwo.ca>
2024-11-22 15:24:09 -0500

    Setting up loop to solve for masses and radii
    - Initialized 2 zeros arrays to be filled with the solved values of mass and radius for each of the 10 unique sets of initial conditions to be solved with solve ivp
    - Added event functionality to solve ivp to extract mass and radii data more accurately (alpha version)

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -18,7 +18,7 @@ initial_mass = 0.
 initial_radius = 1e-10
 
 # beta function definiton
-def odes(r, f, mew_e):
+def odes(r, f):
    mass = f[0]   # setting mass and density as functions odes() in terms of r
    density = f[1]
 
@@ -38,9 +38,21 @@ def zerodensity(r, f):
 zerodensity.terminal = True
 zerodensity.direction = -1
 
+# initializing an empty arrays which can store the solution values mass and radius
+solved_masses = np.zeros(np.size(initial_density))
+solved_radii = np.zeros(np.size(initial_density))
+
+for i in range(np.size(initial_density)):
+   # setting the initial condition
+   f0 = [0., initial_density[i]]
+   soln = integrate.solve_ivp(odes,[initial_radius,100],f0,events=zerodensity)
+   print(soln.t_events)
+   
 
 
-soln = integrate.solve_ivp(odes,[initial_radius,100],[0.,initial_density[0]],args=(2.0,))
+
+
+soln = integrate.solve_ivp(odes,[initial_radius,100],f0)
 print('radius [cm] = '+str(soln.t[-1]))
 print('mass [g] = '+str(soln.y[0,-1]))
 print('end density [cm] = '+str(soln.y[1,-1]))
