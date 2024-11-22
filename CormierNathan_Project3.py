56de5e37105bc77653d7f049ba49d4f7d122b124
ncormie8 <ncormie8@uwo.ca>
2024-11-21 20:52:06 -0500

    Struggling and trying to find any way to make this work
    Doing what I can to try and obtain a dimensionless solution, but I am just so thoroughly confused at this point. I feel as though I have researched and tried everything that I can to get solve ivp to be happy but I just cant seem to find the right answer.

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -8,27 +8,30 @@ import time
 # BC: m(r=0) = 0 , rho(r=0) = rho_c
 # intergrate from 0 (or as close as possible) to rs such that rho(rs) = 0
 
+M0 = 5.67e33/(2.**2)  # [g]
+R0 = 7.72e8/2.    # [cm]
+p0 = 9.74e5*2.  # [g/cm^3]
+
 # 10 inital values of rho to calculate solutions for (given)
-initial_density = np.linspace(1/10.,2.5*(10**6),10)  # already made unitless (p/p0)
+initial_density = np.linspace(p0*1/10.,p0*2.5*(10**6),10)  # already made unitless (p/p0)
 initial_mass = 0.
 initial_radius = 1e-10
 
 # beta function definiton
 def odes(r, f, mew_e):
-   mass = f[0]   # setting mass and density as functions odes() in terms of r
-   density = f[1]
+   mass = f[0]/M0   # setting mass and density as functions odes() in terms of r
+   density = f[1]/p0
 
    x = (density)**(1/3)              # defining x
    gamma = (x**2)/(3*(1+x**2)**(1/2))   # defining gamma in terms of x
 
    # definining differential equations  
-   drho_dr = -(mass)*(density)/gamma*((r)**2)
-   dm_dr = ((r)**2)*(density)
+   drho_dr = -(mass)*(density)/(gamma*((r/R0)**2))
+   dm_dr = ((r/R0)**2)*(density)
    
    return [dm_dr,drho_dr]
 
-soln = integrate.solve_ivp(odes,[initial_radius,100],[0.,initial_density[0]],args=(2.0,))
-print(soln)
+soln = integrate.solve_ivp(odes,[initial_radius,1000000000000],[0.,initial_density[0]],args=(2.0,))
 
 plt.plot(soln.t, soln.y[0,:], 'r', soln.t, soln.y[1,:], 'g')
 plt.xlabel('Radius r')
