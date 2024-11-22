d16736057d50bf3e7519065134ef47db1036b456
ncormie8 <ncormie8@uwo.ca>
2024-11-22 16:32:49 -0500

    Code cleanup + understanding events
    - Removed old clutter code
    - Working on implementing the events functionality properly
    - Added logical stop in odes function definition to not allow negative (non-real) values of density to be used for calculation

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -22,48 +22,37 @@ def odes(r, f):
    mass = f[0]   # setting mass and density as functions odes() in terms of r
    density = f[1]
 
-   x = (density)**(1/3)              # defining x
-   gamma = (x**2)/(3*(1+x**2)**(1/2))   # defining gamma in terms of x
+   # Logic to stop bad values of density being input
+   if density >= 0:
+      x = (density)**(1/3)              # defining x
+      gamma = (x**2)/(3*(1+x**2)**(1/2))   # defining gamma in terms of x
 
-   # definining differential equations  
-   drho_dr = -(mass)*(density)/(gamma*((r)**2))
-   dm_dr = ((r)**2)*(density)
+      # definining differential equations  
+      drho_dr = -(mass)*(density)/(gamma*(r**2))
+      dm_dr = (r**2)*(density)
    
-   return [dm_dr,drho_dr]
+      return [dm_dr,drho_dr]
 
 # setting event condition to stop integration when density is zero
-def zerodensity(r, f):
-   return f[1]
 
-zerodensity.terminal = True
-zerodensity.direction = -1
 
 # initializing an empty arrays which can store the solution values mass and radius
 solved_masses = np.zeros(np.size(initial_density))
 solved_radii = np.zeros(np.size(initial_density))
 
 # setting evaluation time for integration
-t_eval = np.linspace(initial_radius,100,200)
+t_eval = np.linspace(initial_radius,10,1000000000)
 
 for i in range(np.size(initial_density)):
+   def zerodensity(r, f):
+      return f[0]
+   
+   zerodensity.terminal = True
+   zerodensity.direction = -1  
+
    # setting the initial condition
    f0 = [0., initial_density[i]]
    soln = integrate.solve_ivp(odes,[initial_radius,10],f0,events=zerodensity,rtol=1e-8,atol=1e-10)
-   print(soln)
-   # solved_radii[i] = soln.t_events[-1]
-   # solved_masses[i] = soln.y_events[-1]
-   
-
-
-
-
-soln = integrate.solve_ivp(odes,[initial_radius,100],f0)
-print('radius [cm] = '+str(soln.t[-1]))
-print('mass [g] = '+str(soln.y[0,-1]))
-print('end density [cm] = '+str(soln.y[1,-1]))
-
-plt.plot(soln.t, soln.y[0,:], 'r', soln.t, soln.y[1,:], 'g')
-plt.xlabel('Radius r')
-plt.ylabel('Red = m(r) ||||| Green = rho(r)')
-plt.show()
-# ,events=massANDradius
+   print(soln.t_events[0])
+   print(soln.y_events[0])
+   time.sleep(1)
