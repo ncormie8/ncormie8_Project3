2e791ae98a6e1ee4467a84b3af8438dbd8cdff9b
ncormie8 <ncormie8@uwo.ca>
2024-11-21 19:56:33 -0500

    Function updates
    - Removed relativistic constants for dimensionless values as I was not able to achieve real a solution with them included
    - Plotted solutions to dm_dr and drho_dr to verify proper behaviour of solve_ivp with odes()

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -14,19 +14,23 @@ initial_mass = 0.
 initial_radius = 1e-10
 
 # beta function definiton
-def odes(r, f, gamma, mew_e):
-   # constants
-   M0 = 5.67e33/(mew_e**2)  # [g]
-   R0 = 7.72e8/mew_e    # [cm]
-   p0 = 9.74e5*mew_e  # [g/cm^3]
+def odes(r, f, mew_e):
+   mass = f[0]   # setting mass and density as functions odes() in terms of r
+   density = f[1]
 
-   mass, density = f   # setting mass and density as functions odes() in terms of r
+   x = (density)**(1/3)              # defining x
+   gamma = (x**2)/(3*(1+x**2)**(1/2))   # defining gamma in terms of x
 
    # definining differential equations  
-   drho_dr = -(mass/M0)*(density/p0)/gamma*((r/R0)**2)
-   dm_dr = ((r/R0)**2)*(density/p0)
-
+   drho_dr = -(mass)*(density)/gamma*((r)**2)
+   dm_dr = ((r)**2)*(density)
+   
    return [dm_dr,drho_dr]
 
+soln = integrate.solve_ivp(odes,[initial_radius,100],[0.,initial_density[0]],args=(2.0,))
+print(soln)
 
-
+plt.plot(soln.t, soln.y[0,:], 'r', soln.t, soln.y[1,:], 'g')
+plt.xlabel('Radius r')
+plt.ylabel('Red = m(r) ||||| Green = rho(r)')
+plt.show()
