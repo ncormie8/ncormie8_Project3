47390077801b6b528be21cdb7199805d0d0b73cf
ncormie8 <ncormie8@uwo.ca>
2024-11-24 13:41:42 -0500

    Minor updates and commenting
    - Fixed old out of date commenting
    - Added new comments to code that was missing them
    - Added some iteration variable declaration changes for some of the similar arrays used in different questions

--- CormierNathan_Project3.py
+++ CormierNathan_Project3.py
@@ -10,6 +10,7 @@ import time                #used for time.sleep() to debug outputs from poor log
 # BC: m(r=0) = 0 , rho(r=0) = rho_c
 # intergrate from 0 (or as close as possible) to rs such that rho(rs) = 0
 
+# given constant
 ue = 2.
 
 # 10 inital values of rho to calculate solutions for (given)
@@ -19,10 +20,10 @@ initial_radius = 1e-10
 
 # beta function definiton
 def odes(r, f):
-   mass = f[0]   # setting mass and density as functions odes() in terms of r
+   mass = f[0]   # setting mass and density as functions odes() in terms of input f
    density = f[1]
 
-   # Logic to stop bad values of density being input
+   # Logic to stop bad values of density from being used
    if density >= 0:
       x = (density)**(1/3)              # defining x
       gamma = (x**2)/(3*(1+x**2)**(1/2))   # defining gamma in terms of x
@@ -83,23 +84,23 @@ for j in range(np.size(initial_density)):
    plotting_radius = np.multiply(R0,soln2.t[:])
 
    # plotting mass on x, radius on y
-   # plt.plot(plotting_mass,plotting_radius,'-')
-   # plt.xlabel('Mass [g]')
-   # plt.ylabel('Stellar radius [cm]')
-   # plt.title('White dwarf radius in terms of mass for initial density ~'+str(np.round(initial_density[j],2)))
-   # plt.show()
+   plt.plot(plotting_mass,plotting_radius,'-')
+   plt.xlabel('Mass [g]')
+   plt.ylabel('Stellar radius [cm]')
+   plt.title('White dwarf radius in terms of mass for initial density ~'+str(np.round(initial_density[j],2)))
+   plt.show()
 
 # My result for the Chandrasekhar limit was estimated from the plots to be
 # 2.86e33. This is roughly 2 times as large compared to the result obtained by
 # Kippenhahn & Weigert (1990) of 5.836/ue^2 ~= 1.459. This is likely due to
-# the use of poor starting values for the central density of white dwarves
+# the use of inappropriate starting values for the central density of white dwarves
 # used in the integration solution.
 
 # Question 3
 
 # Pick 3 values of initial central density and run solve_ivp again, choosing a
 # different integration method from the one used in part one (RK45). How different
-# are you results?
+# are your results?
 
 # initializing an empty arrays which can store the 
 # solution values mass and radius for quesiton 3
@@ -114,8 +115,8 @@ for k in range(np.size(initial_densities3)):
    # setting the initial condition
    f0 = [0., initial_densities3[k]]
    
-   # solving eqns 8 and 9 for the given initial condition, now using BDF as the integration method
-   soln3 = integrate.solve_ivp(odes,[initial_radius,100],f0,t_eval=t_eval,events=zerodensity,rtol=1e-8,atol=1e-10, method='RK23')
+   # solving eqns 8 and 9 for the given initial condition, now using RK23 as the integration method
+   soln3 = integrate.solve_ivp(odes,[initial_radius,10],f0,t_eval=t_eval,events=zerodensity,rtol=1e-8,atol=1e-10, method='RK23')
    
    # saving the dimensionless values of mass and radius to arrays
    solved_radii3[k] = soln3.t[-1]
@@ -128,9 +129,9 @@ for k in range(np.size(initial_densities3)):
 
 # WRITTEN ANSWER TO QUESTION 3:
 # The results from the method used in question 1 (RK45) when compared to those obtained using
-# the method in question 3 (RK23) are very similar to one another. This is not much of a surprise
-# as they are both explicit Runge-Kutta methods of iterative numerical analysis. While the solutions
-# are practically identical, the RK23 method differs slightly from RK45 for the masses moreso than
+# the method in question 3 (RK23) are very similar. This is not much of a surprise as they are 
+# both explicit Runge-Kutta methods of iterative numerical analysis. While the solutions
+# are practically identical, the RK23 method differs slightly from RK45 for the masses more so than
 # for the radii.
 
 # Question 4
@@ -140,7 +141,7 @@ for k in range(np.size(initial_densities3)):
 
 # Note - Measurements are in units of the Sun's mass and radius
 
-# constants
+# sun constants
 sun_mass = 1.989e33      #[g]
 sun_radius = 69.634e9    # [cm]
 
@@ -148,16 +149,16 @@ sun_radius = 69.634e9    # [cm]
 csv_out = pd.read_csv(r'C:\Users\natha\Desktop\UWO\2024-2025\1st Semester\Physics 3926 - Computer simulations\Python\wd_mass_radius.csv')
 
 # isolating only the needed data into easily manipulable np array
-csv_data = np.array(csv_out) 
+csv_data = np.array(csv_out)
 
 # array data slicing for plotting with unit conversion included
-Msun_wd = csv_data[:,0]
-Msun_unc_wd = csv_data[:,1]
-Rsun_wd = csv_data[:,2]
-Rsun_unc_wd = csv_data[:,3]
+Msun_wd = csv_data[:,0]        # white dwarf sun masses 
+Msun_unc_wd = csv_data[:,1]    # white dwarf sun masses uncertainty
+Rsun_wd = csv_data[:,2]        # white dwarf sun radii
+Rsun_unc_wd = csv_data[:,3]    # white dwarf sun radii uncertainty
 
-# testing our mass-radius relation for the following initial central densities
-test_densities=[1,1.5,2,2.5,3,3.5,4,4.5,5,5.5]
+# testing our mass-radius relation for the following initial central densities (guessed until I found some that looked good)
+test_densities=[0.5,1,1.5,2,2.5,3,3.5,4,4.5,5]
 
 for l in range(np.size(test_densities)):
    
@@ -166,8 +167,8 @@ for l in range(np.size(test_densities)):
    soln4 = integrate.solve_ivp(odes,[initial_radius,10],f0,t_eval=t_eval,events=zerodensity,rtol=1e-8,atol=1e-10)
    
    # extracting the values of mass and radius, and giving them units
-   plotting_mass = np.multiply(M0/sun_mass,soln4.y[0,:])
-   plotting_radius = np.multiply(R0/sun_radius,soln4.t[:])
+   plotting_mass4 = np.multiply(M0/sun_mass,soln4.y[0,:])
+   plotting_radius4 = np.multiply(R0/sun_radius,soln4.t[:])
    
    # loop for plotting all wd values with bidirectional error bars
    for m in range(np.size(Msun_wd)):
@@ -183,8 +184,11 @@ for l in range(np.size(test_densities)):
    
    # plot formatting
    # plotting mass on x, radius on y
-   plt.plot(plotting_mass,plotting_radius,'-')
+   plt.plot(plotting_mass4,plotting_radius4,'-')
    plt.xlabel('Mass [g]')
    plt.ylabel('Stellar radius [cm]')
    plt.title('White dwarf radius in terms of mass for initial density ~'+str(np.round(f0[1],2)))
    plt.show()
+
+# WRITTEN ANSWER TO QUESTION 4:
+# 
